18f5af1542407ffc733cb7abf84d56c5cecc6d39
attajak <janrak@gmail.com>
2024-12-28 10:19:45 +0700

    Initial commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,11 @@
+import "$styles/index.scss"
+import "$styles/syntax-highlighting.css"
+import Bulmatown from "bulmatown"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "$components/**/*.{js,jsx,js.rb,css}"
+
+// Bridgetown quick search
+import "bridgetown-quick-search/dist"
+
+console.info("Bridgetown is loaded!")
